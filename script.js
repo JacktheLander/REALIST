34482ddc62fa10f8eb32a31807a621465f92c7df
Jack Landers <121060192+JacktheLander@users.noreply.github.com>
2024-08-09 09:52:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-<script>
-    function isMobileDevice() {
-        return /Mobi|Android/i.test(navigator.userAgent);
-    }
+function isMobileDevice() {
+    return /Mobi|Android/i.test(navigator.userAgent);
+}
 
-    if (isMobileDevice()) {
-        document.body.classList.add('mobile');
-    }
-</script>
+if (isMobileDevice()) {
+    document.body.classList.add('mobile');
+    document.querySelector('.container').style.height = 'auto'; // Set the container height to auto
+}
