6607ece63958c0f87fafd83f93e12d61205d9c40
Jack Landers <121060192+JacktheLander@users.noreply.github.com>
2024-08-09 09:56:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function isMobileDevice() {
-    return /Mobi|Android/i.test(navigator.userAgent);
+    return (
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+    );
 }
 
 if (isMobileDevice()) {
     document.body.classList.add('mobile');
     document.querySelector('.container').style.height = 'auto'; // Set the container height to auto
+    document.querySelector('.container').style.overflow = 'visible'; // Ensure the container content is fully visible
 }
