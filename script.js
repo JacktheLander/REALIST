b7a70bd4b183b94cd6847de3cc4bfe03e1d0cd89
Jack Landers <121060192+JacktheLander@users.noreply.github.com>
2025-01-05 12:22:06 -0800

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ function isMobileDevice() {
 
 if (isMobileDevice()) {
     document.body.classList.add('mobile');
-    document.querySelector('.container').style.height = 'auto'; // Set the container height to auto
+    document.querySelector('.container').style.height = '20vh'; // Set the container height
     document.querySelector('.container').style.overflow = 'visible'; // Ensure the container content is fully visible
 }
